45a157bea31b662689bb1f592a89cecffb0ad2e2
Felipe Tsuyoshi Suzuki <ftsuyoshisuzuki@gmail.com>
2024-12-04 11:08:45 -0300

    Cria projeto

---
+++ app/src/main/java/com/example/exape/feature/list/ui/ListScreen.kt
@@ -0,0 +1,18 @@
+package com.example.exape.feature.list.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.exape.ui.theme.ExapeTheme
+
+@Composable
+fun ListScreen() {
+
+}
+
+@Composable
+@Preview
+fun ListScreenPreview() {
+    ExapeTheme {
+        ListScreen()
+    }
+}
